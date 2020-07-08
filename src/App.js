077f0b993f8778ba49ceee34ddf52eea5a969a54
fakeboyas <ovoivi9@gmail.com>
2020-07-09 06:12:46 +0700

    finish routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Provider store={createStore(reducers, applyMiddleware(logger, thunk))}>
+        <Router>
+          <Header />
+          <Switch>
+            <Route exact path="/">
+              <Login />
+            </Route>
+            <Route exact path="/">
+              <Login />
+            </Route>
+            <PrivateRoute exact path="/Restaurant">
+              <PokemonList />
+            </PrivateRoute>
+
+            <PrivateRoute exact path="/Restaurant/:name">
+              <PokemonDetails />
+            </PrivateRoute>
+           
+          </Switch>
+        </Router>
+      </Provider>
     </div>
   );
 }
