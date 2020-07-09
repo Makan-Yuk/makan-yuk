f15e9459417dd33d9feed509d51e8fc276fd03c3
JuanBiantong <jbiantong@gmail.com>
2020-07-10 00:29:42 +0700

    fix login styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
-
 import React from "react";
-
 import "./App.css";
-
 import Login from "./Pages/Login/Login";
 import Reducer from "./redux/Reducer";
 import { createStore, applyMiddleware } from "redux";
@@ -10,7 +7,6 @@ import thunk from "redux-thunk";
 import { Provider } from "react-redux";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import logger from "redux-logger";
-import LandingPage from "./Pages/LandingPage/LandingPage";
 
 function App() {
   return (
@@ -20,10 +16,6 @@ function App() {
               <Route exact path="/login">
                   <Login />
               </Route>
-              <Route exact path="/">
-                  <LandingPage />
-              </Route>
-             
           </Switch>
       </Router>
     </Provider>
